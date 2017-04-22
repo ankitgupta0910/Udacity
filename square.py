d706b38dc18f9aa5c647c5b51595dd15cbd21486
Ankit Gupta <ankitgupta09101987@gmail.com>
2017-04-22 08:43:47 -0700

    Square

---
+++ square.py
@@ -0,0 +1,19 @@
+import turtle
+
+
+def draw_square():
+    ank = turtle.Turtle()
+    window = turtle.Screen()
+
+    window.bgcolor("red")
+    for i in range(1,37):
+        for j in range(1,5):
+            ank.forward(100)
+            ank.right(90)
+        ank.right(10)
+
+
+    window.exitonclick()
+
+
+draw_square()
